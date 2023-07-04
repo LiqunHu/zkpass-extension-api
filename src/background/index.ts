5e06dff2de8dc7bfd2acd96ad917d1a17bc3ab36
LiqunHu <huliquns@126.com>
2023-07-04 17:30:10 +0800

    add logic

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,61 +1,175 @@
-// 创建上下文菜单
-const contextMenus = [
-    { id: "addLog", title: "添加日志" },
-    { id: "translate", title: "谷歌翻译" },
-    { id: "play", title: "语音朗读" },
-  ];
-  for (let menu of contextMenus) {
-    chrome.contextMenus.create({
-      id: menu.id,
-      type: "normal",
-      title: menu.title,
-      contexts: ["selection"], // 右键点击选中文字时显示
-      documentUrlPatterns: ["<all_urls>"], // 限制菜单选项仅应用于URL匹配给定模式之一的页面
-    });
+const debug_version = '1.0'
+let genTabId: any = null
+let genWindowId: any = null
+let tabs: { [index: number]: any } = {}
+
+/**
+ * receive content message and new a tab
+ */
+chrome.runtime.onMessage.addListener((request) => {
+  if (request) {
+    if (request.method == 'POPUP') {
+      console.log('chrome.runtime.onMessage POPUP')
+
+      chrome.tabs.create(
+        {
+          url: request.url
+        },
+        (tab: any) => {
+          tabs[tab.id] = {
+            windowId: tab.windowId,
+            requests: new Map()
+          }
+          console.log('in tabs', tabs)
+
+          function allEventHandler(debuggeeId: any, message: any, params: any) {
+            if (!tabs[debuggeeId.tabId]) {
+              return
+            }
+
+            console.log(message)
+
+            if (message == 'Network.requestWillBeSent') {
+              if (params.request) {
+                const detail = new Map()
+                detail.set('request', params.request)
+                tabs[debuggeeId.tabId].requests.set(params.requestId, detail)
+              }
+            }
+
+            // get response data
+            if (message == 'Network.responseReceived') {
+              if (params.response) {
+                const request = tabs[debuggeeId.tabId].requests.get(
+                  params.requestId
+                )
+                if (request === undefined) {
+                  console.log(params.requestId, '#not found request')
+                  return
+                }
+                request.set('response', params.response)
+                tabs[debuggeeId.tabId].requests.set(params.requestId, request)
+              }
+            }
+
+            if (message == 'Network.loadingFinished') {
+              const request = tabs[debuggeeId.tabId].requests.get(
+                params.requestId
+              )
+              if (request === undefined) {
+                console.log(params.requestId, '#not found request')
+                return
+              }
+
+              chrome.debugger.sendCommand(
+                {
+                  tabId: debuggeeId.tabId
+                },
+                'Network.getResponseBody',
+                {
+                  requestId: params.requestId
+                },
+                function (response) {
+                  if (response) {
+                    request.set('response_body', response)
+                    tabs[debuggeeId.tabId].requests.set(
+                      params.requestId,
+                      request
+                    )
+                    console.log(request)
+                    tabs[debuggeeId.tabId].requests.delete(params.requestId)
+                  } else {
+                    console.log('empty')
+                  }
+                }
+              )
+            }
+
+            // if (message == 'Network.responseReceived') {
+            //   //response return
+            //   chrome.debugger.sendCommand(
+            //     {
+            //       tabId: debuggeeId.tabId
+            //     },
+            //     'Network.getResponseBody',
+            //     {
+            //       requestId: params.requestId
+            //     },
+            //     function (response) {
+            //       // you get the response body here!
+            //       // you can close the debugger tips by:
+            //       console.log(response)
+            //       // chrome.debugger.detach(debuggeeId)
+            //     }
+            //   )
+            // }
+          }
+
+          chrome.debugger.attach(
+            {
+              //debug at current tab
+              tabId: tab.id
+            },
+            debug_version,
+            () => {
+              chrome.debugger.sendCommand(
+                {
+                  //first enable the Network
+                  tabId: tab.id
+                },
+                'Network.enable'
+              )
+
+              chrome.debugger.onEvent.addListener(allEventHandler)
+            }
+          )
+        }
+      )
+
+      // chrome.windows.getCurrent((w) => {
+      //   chrome.tabs.query({ active: true, windowId: w.id }, (tabs) => {
+      //     genTabId = tabs[0].id
+      //     genWindowId = w.id
+      //     console.log('request.data', request.data)
+      //     popupWindow(request)
+      //     tabs.push({
+      //       tabId:
+      //     })
+      //   })
+      // })
+    }
   }
-  
-  // 监听上下文菜单点击事件
-  chrome.contextMenus.onClicked.addListener((info) => {
-    if (info.selectionText) {
-      switch (info.menuItemId) {
-        case "addLog":
-          // 查询当前打开的标签页
-          console.log('addLog')
-          chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            // 向当前打开的标签页发送添加日志的消息
-            chrome.tabs.sendMessage(tabs[0].id||0, {
-              todo: "addLog",
-              data: info.selectionText,
-            });
-          });
-          break;
-        case "translate":
-          // 打开谷歌翻译页面
-          chrome.windows.create({
-            url: `https://translate.google.cn/?sl=zh-CN&tl=en&text=${info.selectionText}&op=translate`,
-            type: "popup",
-            top: 10,
-            left: 10,
-            width: 800,
-            height: 500,
-          });
-          break;
-        case "play":
-          // 语音朗读
-          chrome.tts.speak(info.selectionText, { rate: 1 }); // rate配置项可以修改语音朗读速度，1为正常速度
-          break;
-        default:
-          break;
-      }
-    }
-  });
-  
-  // 监听content_scripts页面发来的消息
-  chrome.runtime.onMessage.addListener((request) => {
-    console.log("接收到content_scripts消息：", request);
-    if (request.todo === "saveLog") {
-      console.log('saveLog')
-    }
-  });
-  
-  
+  return true
+})
+
+// function popupWindow(request: any) {
+//   if (chrome) {
+//     chrome.webRequest.onCompleted.addListener(
+//       function (details) {
+//         console.log(details)
+//       },
+//       { urls: ['<all_urls>'], types: ["xmlhttprequest"]},
+//       ["responseHeaders","extraHeaders"]
+//     )
+//     chrome.tabs.create(
+//       {
+//         url: request.url
+//       },
+//       (tab) => {
+//         console.log('tab', tab)
+//       }
+//     )
+//   }
+// }
+
+chrome.tabs.onRemoved.addListener(function (tabId) {
+  console.log('removed', tabId)
+
+  if (Object.keys(tabs).indexOf(tabId + '') >= 0) {
+    chrome.debugger.detach({
+      tabId: tabId
+    })
+    delete tabs[tabId]
+    console.log('out tabs', tabs)
+  }
+})
--- src/content/content.tsx
+++ src/content/content.tsx
@@ -1,23 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import '@/fonts/JetBrainsMono-Light.ttf'
-import '@/fonts/JetBrainsMono-Regular.ttf'
-import '@/fonts/JetBrainsMono-Medium.ttf'
-import '@/fonts/JetBrainsMono-ExtraBold.ttf'
-
-import './App.css'
-
-const mask = document.createElement('div')
-mask.className = 'plugin_root'
-mask.setAttribute('id', 'plugin-mask')  
-// document.body.insertBefore(mask, document.body.firstChild)
-document.body.appendChild(mask)
-
-const root = ReactDOM.createRoot(mask)
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+window.addEventListener('message', (event) => {
+  if (event.source != window) return
+  if (
+    event.data.type &&
+    event.data.type === 'ZKPASS_EXTENSION' &&
+    (event.origin.includes('localhost') || event.origin.includes('zkpass.org'))
+  ) {
+    console.log(event)
+    chrome.runtime.sendMessage(event.data.doc)
+  }
+})
